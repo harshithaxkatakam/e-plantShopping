1096ba0b489f2e35a295e45b20720771ba878953
harshithaxkatakam <harshithaxkatakam@gmail.com>
2025-03-17 05:35:38 -0400

    deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
   plugins: [react()],
+  base: '/e-plantShopping/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+  },
 })
